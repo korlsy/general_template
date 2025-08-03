5be15b72ed57270aaa486b969839cdb62a79c80b
sangyeol.lee <korlsy@gmail.com>
2025-08-03 17:24:26 +0900

    c1

--- java/java_template/java/general_templates/JavaTemplate.java
+++ java/java_template/java/general_templates/JavaTemplate.java
@@ -2,10 +2,10 @@ package general_templates;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+
 public class JavaTemplate {
 
 	public static void main(String[] args) {
 		System.out.println( RandomStringUtils.randomAlphanumeric(20) );
 	}
-
 }
